d8f330924caacb9a68d5ea04581c54bfd448441e
plohish2009 <artem.rusakov.0606@mail.ru>
2025-10-06 17:40:02 +0300

    improve a liitle

--- scnd_scr.sh
+++ scnd_scr.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
 
-# Скрипт для тестирования doesitwork.sh с тремя разными наборами параметров
-
-echo "=== ТЕСТ 1 ==="
+echo "=== ТЕСТ 1 ===" # expecting output: there is no such directory
 {
-    echo "test_folder_1"    # путь к директории
-    echo "10"            # размер образа в MB
-    echo "50"            # лимит использования в %
-} | ./deepseek.sh
+    echo "test_folder"   
+    echo "10"            
+    echo "50"            
+} | ./frst_scr.sh
 
 echo ""
-echo "=== ТЕСТ 2 ==="
+echo "=== ТЕСТ 2 ===" #no framed, no files inside
 {
-    echo "test_folder_2"   # путь к директории  
-    echo "20"            # размер образа в MB
-    echo "70"            # лимит использования в %
-} | ./deepseek.sh
+    echo "test_folder_2"    
+    echo "10"           
+    echo "70"            
+} | ./frst_scr.sh
 
 echo ""
-echo "=== ТЕСТ 3 ==="
+echo "=== ТЕСТ 3 ===" #no framed files inside
 {
-    echo "test_folder_3"    # путь к директории
-    echo "15"            # размер образа в MB
-    echo "30"            # лимит использования в %
-} | ./deepseek.sh
+    echo "test_folder_3"    
+    echo "10"           
+    echo "30"          
+} | ./frst_scr.sh
 
+echo "=== ТЕСТ 4 ===" #framed, do not need archieve
+{
+    echo "test_folder_4"   
+    echo "15"            
+    echo "30"           
+} | ./frst_scr.sh
+
+echo "=== ТЕСТ 5 ===" #framed, need archieve
+{
+    echo "test_folder_5"    
+    echo "15"            
+    echo "30"            
+} | ./frst_scr.sh
